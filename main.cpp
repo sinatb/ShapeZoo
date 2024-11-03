a044e108538157089e1ed6c66fda3131bf40d32b
sinatb <sinatb.dev@gmail.com>
2024-11-03 17:51:29 +0330

    collision-002 : Collision for Rectangles added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "include/Triangle.h"
+#include "include/Circle.h"
+#include "include/Rectangle.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Shape Zoo");
+    std::string text = "Text";
+    Rectangle r(text, sf::Vector2f(0, 0), sf::Vector2f(0.1, 0.1), sf::Color::Red, 30,30);
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color::Blue);
+        r.Update(window);
+        window.display();
+    }
 }
