e102d2571f0cbb1a1b8163071a8fdaf7ccb2a486
sinatb <sinatb.dev@gmail.com>
2024-11-03 17:52:08 +0330

    Revert "collision-002 : Collision for Rectangles added"
    This reverts commit a044e108538157089e1ed6c66fda3131bf40d32b.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
-#include <SFML/Graphics.hpp>
-#include "include/Triangle.h"
-#include "include/Circle.h"
-#include "include/Rectangle.h"
+#include <iostream>
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "Shape Zoo");
-    std::string text = "Text";
-    Rectangle r(text, sf::Vector2f(0, 0), sf::Vector2f(0.1, 0.1), sf::Color::Red, 30,30);
-    while (window.isOpen())
-    {
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Blue);
-        r.Update(window);
-        window.display();
-    }
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
 }
