6aa56ff4670b14f5bf546e2b36ad1137eddaedb0
sinatb <sinatb.dev@gmail.com>
2024-11-03 12:11:30 +0330

    shapes-003 : Added base shape random direction generator

--- include/BaseShape.h
+++ include/BaseShape.h
@@ -5,25 +5,46 @@
 #ifndef SHAPEZOO_BASESHAPE_H
 #define SHAPEZOO_BASESHAPE_H
 
+#include <cmath>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include <random>
 class BaseShape {
-    std::string m_text;
+
+void Move(){
+    m_position += m_speed;
+}
+static sf::Vector2f getRandomDirection() {
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(0.0,1.0);
+
+    float rand_x =  dist(gen);
+    float rand_y = dist(gen);
+    float div = std::sqrt(rand_x*rand_x + rand_y*rand_y);
+    return {rand_x/div, rand_y/div};
+
+}
+protected:
+    std::string  m_text;
     sf::Vector2f m_position;
     sf::Vector2f m_speed;
-    void Move(){
-        m_position += m_speed;
-    }
+    sf::Color    m_color;
+    sf::Vector2f m_direction;
+
 public:
-    BaseShape(std::string& text, sf::Vector2f position, sf::Vector2f speed):
+    BaseShape(std::string& text, sf::Vector2f& position, sf::Vector2f& speed, sf::Color& color):
         m_text(text),
         m_position(position),
-        m_speed(speed)
+        m_speed(speed),
+        m_color(color),
+        m_direction(getRandomDirection())
     {}
-    virtual void OnCollision() = 0;
+    virtual void OnCollision(sf::RenderWindow &window) = 0;
     virtual void Draw(sf::RenderWindow &window) = 0;
     void Update(sf::RenderWindow &window) {
-        OnCollision();
+        OnCollision(window);
         Move();
         Draw(window);
     }
