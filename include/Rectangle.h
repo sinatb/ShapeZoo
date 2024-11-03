22e0f673395591b4873ac7aad6a94b57de817615
sinatb <sinatb.dev@gmail.com>
2024-11-03 12:47:31 +0330

    shapes-006 : Seperated definition and declaration of rectangle

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -13,18 +13,8 @@ float m_width;
 float m_height;
 sf::RectangleShape m_rectangle;
 public:
-    Rectangle(std::string& text,sf::Vector2f position, sf::Vector2f speed, sf::Color color,float width, float height) :
-            BaseShape(text, position, speed, color, m_rectangle),m_width(width), m_height(height)
-    {
-        m_rectangle = sf::RectangleShape(sf::Vector2f(m_width, m_height));
-        m_shape.setPosition(m_position);
-        m_rectangle.setFillColor(m_color);
-    }
-    void OnCollision(sf::RenderWindow &window) override {
-
-    }
-    void Draw(sf::RenderWindow &window) override {
-        window.draw(m_rectangle);
-    }
+    Rectangle(std::string& text,sf::Vector2f position, sf::Vector2f speed, sf::Color color,float width, float height);
+    void OnCollision(sf::RenderWindow &window) override;
+    void Draw(sf::RenderWindow &window) override;
 };
 #endif //SHAPEZOO_RECTANGLE_H
